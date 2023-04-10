6d85951693b74a58d4ede94d2dd27d724c6db0e6
Orozbek97 <tagaev.orozbek@gmail.com>
2023-04-11 00:47:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import DeleteIcon from "../components/icons/delete-icon";
+import EditIcon from "../components/icons/edit-icon";
 
 function App() {
+    const [todoList , setTodoList] = useState((list))
+    const [todo , setTodo] = useState('')
+
+    const handleAddTodo = () => {
+        if (todo.length) {
+            const newTodo = {
+                id: todoList.length + 1,
+                text: todo,
+                completed: false
+            }
+            setTodo('')
+            setTodoList([...todoList, newTodo])
+        }
+    }
+    const handleDelete = (id) => {
+        setTodoList(todoList.filter(todo => todo.id !== id))
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'wrapper'}>
+        <div>
+            <div className={'input-wrapper'}>
+                <input
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                    type="text"
+                />
+                <button className={'add-btn'} onClick={handleAddTodo}>
+                    add
+                </button>
+            </div>
+            {
+                todoList.map((todo) => (
+                    <div key={todo.id} className={'todo-wrapper'}>
+                        <span> {todo.text}</span>
+                    <div>
+                    <button className={'edit-btn'}>
+                        <EditIcon/>
+                    </button>
+                     <button className={'edit-btn'}
+                     onClick={() => handleDelete(todo.id)}>
+                         <DeleteIcon/>
+                     </button>
+                    </div>
+                    </div>
+                  ))
+            }
+        </div>
     </div>
-  );
-}
+                )
+
+            }
 
 export default App;
+
+const list = [
+    {id: 1, text: 'Learn React', completed: false},
+    {id: 2, text: 'Learn Firebase', completed: false},
+    {id: 3, text: 'Learn GraphQL', completed: false},
+    {id: 4, text: 'Learn React Native', completed: false},
+]
